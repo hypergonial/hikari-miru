1494acb8b178f143c26e6be615733f475707f76d
HyperGH <46067571+HyperGH@users.noreply.github.com>
2022-02-06 21:18:14 +0100

    Actually fix StopButton

--- miru/ext/nav/buttons.py
+++ miru/ext/nav/buttons.py
@@ -237,7 +237,7 @@ class StopButton(NavButton[NavigatorViewT]):
         super().__init__(style=style, label=label, custom_id=custom_id, emoji=emoji, row=row)
 
     async def callback(self, context: Context) -> None:
-        if not self.view.message or not self.view._inter:
+        if not self.view.message and not self.view._inter:
             return
 
         for button in self.view.children:
