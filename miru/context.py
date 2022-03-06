73c7ad5f934ecfadba8e1228f9383b35bd4a2f01
HyperGH <46067571+HyperGH@users.noreply.github.com>
2022-03-06 20:03:50 +0100

    Add overloads for Context.defer()

--- miru/context.py
+++ miru/context.py
@@ -23,7 +23,7 @@
 from __future__ import annotations
 
 import abc
-import typing
+import typing as t
 
 import hikari
 from hikari.snowflakes import Snowflake
@@ -34,16 +34,16 @@ from .interaction import ComponentInteraction
 from .interaction import ModalInteraction
 from .traits import MiruAware
 
-if typing.TYPE_CHECKING:
+if t.TYPE_CHECKING:
     from .modal import Modal
     from .view import View
 
 __all__ = ["Context", "ViewContext", "ModalContext", "RawContext"]
 
-InteractionT = typing.TypeVar("InteractionT", "ComponentInteraction", "ModalInteraction")
+InteractionT = t.TypeVar("InteractionT", "ComponentInteraction", "ModalInteraction")
 
 
-class Context(abc.ABC, typing.Generic[InteractionT]):
+class Context(abc.ABC, t.Generic[InteractionT]):
     """An abstract base class for context
     objects that proxying a Discord interaction."""
 
@@ -79,7 +79,7 @@ class Context(abc.ABC, typing.Generic[InteractionT]):
         return self.user
 
     @property
-    def member(self) -> typing.Optional[hikari.InteractionMember]:
+    def member(self) -> t.Optional[hikari.InteractionMember]:
         """The member who triggered this interaction. Will be None in DMs."""
         return self._interaction.member
 
@@ -89,7 +89,7 @@ class Context(abc.ABC, typing.Generic[InteractionT]):
         return self._interaction.locale
 
     @property
-    def guild_locale(self) -> typing.Optional[str]:
+    def guild_locale(self) -> t.Optional[str]:
         """
         The guild locale of this context, if in a guild.
         This will default to `en-US` if not a community guild.
@@ -102,20 +102,20 @@ class Context(abc.ABC, typing.Generic[InteractionT]):
         return self._interaction.channel_id
 
     @property
-    def guild_id(self) -> typing.Optional[Snowflake]:
+    def guild_id(self) -> t.Optional[Snowflake]:
         """The ID of the guild the context represents. Will be None in DMs."""
         return self._interaction.guild_id
 
-    def get_guild(self) -> typing.Optional[hikari.GatewayGuild]:
+    def get_guild(self) -> t.Optional[hikari.GatewayGuild]:
         """Gets the guild this context represents, if any. Requires application cache."""
         return self._interaction.get_guild()
 
-    def get_channel(self) -> typing.Optional[hikari.TextableGuildChannel]:
+    def get_channel(self) -> t.Optional[hikari.TextableGuildChannel]:
         """Gets the channel this context represents, None if in a DM. Requires application cache."""
         return self._interaction.get_channel()
 
     async def fetch_response(self) -> hikari.Message:
-        """Fetch the response message.
+        """Fetch the initial response message.
 
         Returns
         -------
@@ -133,51 +133,51 @@ class Context(abc.ABC, typing.Generic[InteractionT]):
 
     async def respond(
         self,
-        content: hikari.UndefinedOr[typing.Any] = hikari.UNDEFINED,
+        content: hikari.UndefinedOr[t.Any] = hikari.UNDEFINED,
         *,
-        flags: typing.Union[int, hikari.MessageFlag, hikari.UndefinedType] = hikari.UNDEFINED,
+        flags: t.Union[int, hikari.MessageFlag, hikari.UndefinedType] = hikari.UNDEFINED,
         tts: hikari.UndefinedOr[bool] = hikari.UNDEFINED,
         component: hikari.UndefinedOr[hikari.api.ComponentBuilder] = hikari.UNDEFINED,
-        components: hikari.UndefinedOr[typing.Sequence[hikari.api.ComponentBuilder]] = hikari.UNDEFINED,
+        components: hikari.UndefinedOr[t.Sequence[hikari.api.ComponentBuilder]] = hikari.UNDEFINED,
         attachment: hikari.UndefinedOr[hikari.Resourceish] = hikari.UNDEFINED,
-        attachments: hikari.UndefinedOr[typing.Sequence[hikari.Resourceish]] = hikari.UNDEFINED,
+        attachments: hikari.UndefinedOr[t.Sequence[hikari.Resourceish]] = hikari.UNDEFINED,
         embed: hikari.UndefinedOr[hikari.Embed] = hikari.UNDEFINED,
-        embeds: hikari.UndefinedOr[typing.Sequence[hikari.Embed]] = hikari.UNDEFINED,
+        embeds: hikari.UndefinedOr[t.Sequence[hikari.Embed]] = hikari.UNDEFINED,
         mentions_everyone: hikari.UndefinedOr[bool] = hikari.UNDEFINED,
         user_mentions: hikari.UndefinedOr[
-            typing.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]
+            t.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]
         ] = hikari.UNDEFINED,
         role_mentions: hikari.UndefinedOr[
-            typing.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]
+            t.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]
         ] = hikari.UNDEFINED,
     ) -> None:
         """Short-hand method to respond to the interaction this context represents.
 
         Parameters
         ----------
-        content : undefined.UndefinedOr[typing.Any], optional
+        content : undefined.UndefinedOr[t.Any], optional
             The content of the message. Anything passed here will be cast to str.
         tts : undefined.UndefinedOr[bool], optional
             If the message should be tts or not.
         attachment : undefined.UndefinedOr[hikari.Resourceish], optional
             An attachment to add to this message.
-        attachments : undefined.UndefinedOr[typing.Sequence[hikari.Resourceish]], optional
+        attachments : undefined.UndefinedOr[t.Sequence[hikari.Resourceish]], optional
             A sequence of attachments to add to this message.
         component : undefined.UndefinedOr[hikari.api.special_endpoints.ComponentBuilder], optional
             A component to add to this message.
-        components : undefined.UndefinedOr[typing.Sequence[hikari.api.special_endpoints.ComponentBuilder]], optional
+        components : undefined.UndefinedOr[t.Sequence[hikari.api.special_endpoints.ComponentBuilder]], optional
             A sequence of components to add to this message.
         embed : undefined.UndefinedOr[hikari.Embed], optional
             An embed to add to this message.
-        embeds : undefined.UndefinedOr[typing.Sequence[hikari.Embed]], optional
+        embeds : undefined.UndefinedOr[t.Sequence[hikari.Embed]], optional
             A sequence of embeds to add to this message.
         mentions_everyone : undefined.UndefinedOr[bool], optional
             If True, mentioning @everyone will be allowed.
-        user_mentions : undefined.UndefinedOr[typing.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]], optional
+        user_mentions : undefined.UndefinedOr[t.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]], optional
             The set of allowed user mentions in this message. Set to True to allow all.
-        role_mentions : undefined.UndefinedOr[typing.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]], optional
+        role_mentions : undefined.UndefinedOr[t.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]], optional
             The set of allowed role mentions in this message. Set to True to allow all.
-        flags : typing.Union[undefined.UndefinedType, int, hikari.MessageFlag], optional
+        flags : t.Union[undefined.UndefinedType, int, hikari.MessageFlag], optional
             Message flags that should be included with this message.
         """
         if self.interaction._issued_response:
@@ -214,58 +214,53 @@ class Context(abc.ABC, typing.Generic[InteractionT]):
 
     async def edit_response(
         self,
-        content: hikari.UndefinedOr[typing.Any] = hikari.UNDEFINED,
+        content: hikari.UndefinedOr[t.Any] = hikari.UNDEFINED,
         *,
-        flags: typing.Union[int, hikari.MessageFlag, hikari.UndefinedType] = hikari.UNDEFINED,
+        flags: t.Union[int, hikari.MessageFlag, hikari.UndefinedType] = hikari.UNDEFINED,
         tts: hikari.UndefinedOr[bool] = hikari.UNDEFINED,
         component: hikari.UndefinedOr[hikari.api.ComponentBuilder] = hikari.UNDEFINED,
-        components: hikari.UndefinedOr[typing.Sequence[hikari.api.ComponentBuilder]] = hikari.UNDEFINED,
+        components: hikari.UndefinedOr[t.Sequence[hikari.api.ComponentBuilder]] = hikari.UNDEFINED,
         attachment: hikari.UndefinedOr[hikari.Resourceish] = hikari.UNDEFINED,
-        attachments: hikari.UndefinedOr[typing.Sequence[hikari.Resourceish]] = hikari.UNDEFINED,
+        attachments: hikari.UndefinedOr[t.Sequence[hikari.Resourceish]] = hikari.UNDEFINED,
         embed: hikari.UndefinedOr[hikari.Embed] = hikari.UNDEFINED,
-        embeds: hikari.UndefinedOr[typing.Sequence[hikari.Embed]] = hikari.UNDEFINED,
+        embeds: hikari.UndefinedOr[t.Sequence[hikari.Embed]] = hikari.UNDEFINED,
         replace_attachments: bool = False,
         mentions_everyone: hikari.UndefinedOr[bool] = hikari.UNDEFINED,
         user_mentions: hikari.UndefinedOr[
-            typing.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]
+            t.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]
         ] = hikari.UNDEFINED,
         role_mentions: hikari.UndefinedOr[
-            typing.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]
+            t.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]
         ] = hikari.UNDEFINED,
     ) -> None:
-        """A short-hand method to edit the message belonging to this interaction.
+        """A short-hand method to edit the last response belonging to this interaction.
 
         Parameters
         ----------
-        content : undefined.UndefinedOr[typing.Any], optional
+        content : undefined.UndefinedOr[t.Any], optional
             The content of the message. Anything passed here will be cast to str.
         tts : undefined.UndefinedOr[bool], optional
             If the message should be tts or not.
         attachment : undefined.UndefinedOr[hikari.Resourceish], optional
             An attachment to add to this message.
-        attachments : undefined.UndefinedOr[typing.Sequence[hikari.Resourceish]], optional
+        attachments : undefined.UndefinedOr[t.Sequence[hikari.Resourceish]], optional
             A sequence of attachments to add to this message.
         component : undefined.UndefinedOr[hikari.api.special_endpoints.ComponentBuilder], optional
             A component to add to this message.
-        components : undefined.UndefinedOr[typing.Sequence[hikari.api.special_endpoints.ComponentBuilder]], optional
+        components : undefined.UndefinedOr[t.Sequence[hikari.api.special_endpoints.ComponentBuilder]], optional
             A sequence of components to add to this message.
         embed : undefined.UndefinedOr[hikari.Embed], optional
             An embed to add to this message.
-        embeds : undefined.UndefinedOr[typing.Sequence[hikari.Embed]], optional
+        embeds : undefined.UndefinedOr[t.Sequence[hikari.Embed]], optional
             A sequence of embeds to add to this message.
         mentions_everyone : undefined.UndefinedOr[bool], optional
             If True, mentioning @everyone will be allowed.
-        user_mentions : undefined.UndefinedOr[typing.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]], optional
+        user_mentions : undefined.UndefinedOr[t.Union[hikari.SnowflakeishSequence[hikari.PartialUser], bool]], optional
             The set of allowed user mentions in this message. Set to True to allow all.
-        role_mentions : undefined.UndefinedOr[typing.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]], optional
+        role_mentions : undefined.UndefinedOr[t.Union[hikari.SnowflakeishSequence[hikari.PartialRole], bool]], optional
             The set of allowed role mentions in this message. Set to True to allow all.
-        flags : typing.Union[undefined.UndefinedType, int, hikari.MessageFlag], optional
+        flags : t.Union[undefined.UndefinedType, int, hikari.MessageFlag], optional
             Message flags that should be included with this message.
-
-        Raises
-        ------
-        RuntimeError
-            The interaction was already responded to.
         """
         if self.interaction._issued_response:
             await self.interaction.edit_initial_response(
@@ -298,19 +293,50 @@ class Context(abc.ABC, typing.Generic[InteractionT]):
                 flags=flags,
             )
 
-    async def defer(self, flags: typing.Union[int, hikari.MessageFlag, None] = None) -> None:
+    @t.overload
+    async def defer(
+        self,
+        response_type: hikari.ResponseType,
+        *,
+        flags: hikari.UndefinedOr[t.Union[int, hikari.MessageFlag]] = hikari.UNDEFINED,
+    ) -> None:
+        ...
+
+    @t.overload
+    async def defer(self, *, flags: hikari.UndefinedOr[t.Union[int, hikari.MessageFlag]] = hikari.UNDEFINED) -> None:
+        ...
+
+    async def defer(
+        self,
+        *args: t.Any,
+        flags: hikari.UndefinedOr[t.Union[int, hikari.MessageFlag]] = hikari.UNDEFINED,
+        **kwargs: t.Any,
+    ) -> None:
         """Short-hand method to defer an interaction response. Raises RuntimeError if the interaction was already responded to.
 
         Parameters
         ----------
-        flags : typing.Union[int, hikari.MessageFlag, None], optional
+        response_type : hikari.ResponseType, optional
+            The response-type of this defer action. Defaults to DEFERRED_MESSAGE_UPDATE.
+        flags : t.Union[int, hikari.MessageFlag, None], optional
             Message flags that should be included with this defer request, by default None
 
         Raises
         ------
         RuntimeError
             The interaction was already responded to.
+        ValueError
+            response_type was not a deffered response type.
         """
+        response_type = args[0] if args else hikari.ResponseType.DEFERRED_MESSAGE_UPDATE
+
+        if response_type not in [
+            hikari.ResponseType.DEFERRED_MESSAGE_CREATE,
+            hikari.ResponseType.DEFERRED_MESSAGE_UPDATE,
+        ]:
+            raise ValueError(
+                "Parameter response_type must be ResponseType.DEFERRED_MESSAGE_CREATE or ResponseType.DEFERRED_MESSAGE_UPDATE."
+            )
 
         if self.interaction._issued_response:
             raise RuntimeError("Interaction was already responded to.")
@@ -335,8 +361,8 @@ class ViewContext(Context[ComponentInteraction]):
         return self._view
 
     @property
-    def message(self) -> typing.Optional[hikari.Message]:
-        """The message object this context is proxying."""
+    def message(self) -> t.Optional[hikari.Message]:
+        """The message object for the view this context is proxying."""
         return self._interaction.message
 
     async def respond_with_modal(self, modal: Modal) -> None:
@@ -352,7 +378,7 @@ class ViewContext(Context[ComponentInteraction]):
 class ModalContext(Context[ModalInteraction]):
     """A context object proxying a ModalInteraction."""
 
-    def __init__(self, modal: Modal, interaction: ModalInteraction, values: typing.Dict[ModalItem[Modal], str]) -> None:
+    def __init__(self, modal: Modal, interaction: ModalInteraction, values: t.Dict[ModalItem[Modal], str]) -> None:
         super().__init__(interaction)
         self._modal = modal
         self._values = values
@@ -363,6 +389,6 @@ class ModalContext(Context[ModalInteraction]):
         return self._modal
 
     @property
-    def values(self) -> typing.Dict[ModalItem[Modal], str]:
+    def values(self) -> t.Dict[ModalItem[Modal], str]:
         """The values received as input for this modal."""
         return self._values
